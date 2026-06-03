40e9854c8e1f0afa8566d18d9d1e249c8b710083
carolyn999 <143912444+carolyn999@users.noreply.github.com>
2026-06-03 16:00:48 -0600

    DELETE

--- main.tf
+++ main.tf
@@ -49,14 +49,3 @@ resource "aws_security_group_rule" "blog_https_in" {
 
   security_group_id = aws_security_group.blog.id
 }
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["10.0.0.0/8"] 
-
-  security_group_id = aws_security_group.blog.id
-}
-#test
